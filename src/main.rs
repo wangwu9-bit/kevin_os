a238518d760ec5078180a9b443d2099b35eec18a
kevin_wang <ltwoggbuty2mh@gmail.com>
2025-02-05 08:34:03 +0800

    fix no main error

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 #![no_std]
-
+#![no_main]
 use core::panic::PanicInfo;
-fn main() {
-
-}
-
 
 #[panic_handler]
 fn panic_(_info:&PanicInfo)->!{
+    loop {
+        
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn _start()->!{
     loop {
         
     }
