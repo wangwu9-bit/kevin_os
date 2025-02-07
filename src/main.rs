b71fb1b5087fd44a5488f53711a100f771a3f677
kevin_wang <ltwoggbuty2mh@gmail.com>
2025-02-07 23:27:48 +0800

    build a kernel disk image

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,23 @@ fn panic(_info:&PanicInfo)->!{
     }
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
-pub extern "C" fn _start()->!{
-    loop {
-        
+pub extern "C" fn _start() -> ! {
+    let vbg_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vbg_buffer.offset(i as isize * 2) = byte;
+            *vbg_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
     }
+
+    unsafe {
+        *vbg_buffer.offset(0) = b'M';    // 写入字符
+        *vbg_buffer.offset(1) = 0x0F;    // 设置为白色
+    }
+
+    loop {}
 }
